4ece72757441e1b65e99606afaf33fed1446a72d
ErnestoAMujica <ernesto.a.mujica@gmail.com>
2022-10-28 15:02:37 -0400

    Testing a commit for the setup doc. Comment can be removed later.

--- app/src/main/java/com/example/myemissions/MainActivity.java
+++ app/src/main/java/com/example/myemissions/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Commit push test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

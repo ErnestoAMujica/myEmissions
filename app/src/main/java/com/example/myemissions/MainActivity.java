9c744b915506085351521274bc4a8ba116f3dfcb
Syed Mahdi <80363325+5yM01@users.noreply.github.com>
2022-11-01 17:48:36 -0400

    Created basic login page by editing XML file

--- app/src/main/java/com/example/myemissions/MainActivity.java
+++ app/src/main/java/com/example/myemissions/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myemissions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //called when user presses the login button in login page
+    public void transferHome(View view) {
+        // send user to the home page...
+    }
+
 }
